0a435f19045f7ab0ca440826cc42c6cd810550be
Manshi1201 <manshi75kumari@gmail.com>
2026-05-09 22:29:17 +0530

    Initial commit

---
+++ src/test/java/tests/SauceDemoLoginTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SauceDemoLoginTest {
+
+	private WebDriver driver;
+	  private Map<String, Object> vars;
+	  JavascriptExecutor js;
+	  @BeforeMethod
+	  public void setUp() {
+	    driver = new ChromeDriver();
+	    js = (JavascriptExecutor) driver;
+	    vars = new HashMap<String, Object>();
+	  }
+	  @AfterMethod
+	  public void tearDown() {
+	    driver.quit();
+	  }
+	  @Test
+	  public void sauceDemoLoginTest() {
+	    driver.get("https://www.saucedemo.com/");
+	    driver.manage().window().setSize(new Dimension(831, 700));
+	    driver.findElement(By.cssSelector("*[data-test=\"username\"]")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
+	    driver.findElement(By.cssSelector("*[data-test=\"password\"]")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+	    driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"add-to-cart-sauce-labs-backpack\"]")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"add-to-cart-sauce-labs-bike-light\"]")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]")).click();
+	    driver.findElement(By.id("react-burger-menu-btn")).click();
+	    driver.findElement(By.cssSelector("*[data-test=\"logout-sidebar-link\"]")).click();
+	  }
+}
